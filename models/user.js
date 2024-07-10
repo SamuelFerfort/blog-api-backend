1ff515f25641cee46e487a653e71d21adaf73637
Samuel <samuferfort@gmail.com>
2024-07-10 21:39:32 +0200

    add name field to schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["author", "user"], default: "user" },
