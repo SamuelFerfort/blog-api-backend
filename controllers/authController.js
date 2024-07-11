dae5f6c401e14e4914f507606a43473828514b05
Samuel <samuferfort@gmail.com>
2024-07-11 11:06:55 +0200

    add loginUser and registerUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,37 @@
-import User from "../models/user"
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import "dotenv/config"
 
 
+export const registerUser = async (req, res) => {
+  const emailExist = await User.findOne({ email: req.body.email });
+
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+
+  try {
+    await user.save();
+    res.status(200).send("Registration successful");
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email does not exist");
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Wrong Password");
+
+  const token = jwt.sign({ id: user._id}, process.env.TOKEN_SECRET)
+  res.header("auth-token", token).send(token)
+};
