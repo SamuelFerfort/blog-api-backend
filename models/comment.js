02bf06c8a6c4d1987868dfdbae6015603d8f4db3
Samuel <samuferfort@gmail.com>
2024-07-12 20:47:08 +0200

    adjust comment schema

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,6 @@ const { Schema } = mongoose;
 const CommentSchema = new Schema({
   post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   author: { type: Schema.Types.ObjectId, ref: "User" },
-  email: { type: String, required: true },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
