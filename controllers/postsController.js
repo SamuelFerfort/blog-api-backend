c86246a7fefe5c2de93392d428656f9971426a01
Samuel <samuferfort@gmail.com>
2024-07-13 22:33:51 +0200

    populate author on post get

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,7 +2,7 @@ import Post from "../models/post.js";
 
 export const getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find({published: true});
+    const posts = await Post.find({ published: true });
 
     if (!posts) return res.status(404).json({ message: "No posts found" });
 
@@ -13,16 +13,18 @@ export const getAllPosts = async (req, res) => {
   }
 };
 
-export const getPostById = async(req, res) => {
+export const getPostById = async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id).populate({
+      path: "author",
+      select: "name",
+    });
     if (!post) return res.status(404).json({ message: "No posts found" });
-    res.json(post)
+    res.json(post);
   } catch (err) {
-    console.error("Error getting post", err)
+    console.error("Error getting post", err);
     res.status(500).json({ message: "Internal server error" });
-
   }
-}
+};
 
 export const createPost = async (req, res) => {};
